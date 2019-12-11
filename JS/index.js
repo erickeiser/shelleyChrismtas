6c48b50e3a14b6e1648c6a1b53575e6a601bb551
Eric Keiser <ekeiser@ekeiser.lan.tombras.com>
2019-12-11 12:51:13 -0500

    jQuery

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-    $(".hero").hide()
-    $(".secondary").hide()
-    $(".button").hide()
+  
 
-    $(".hero").delay(2000).fadeIn(1500).delay(2000).fadeOut(1500)
-    $(".secondary").delay(7500).fadeIn().delay(2000).fadeOut()
-    $(".button").delay(10000).fadeIn(1500)
+    $(".hero").delay(2000).fadeIn(3000).delay(2000).fadeOut(1500)
+    $(".secondary").delay(9500).fadeIn(3000).delay(3000).fadeOut(1500)
+    $(".button").delay(17500).fadeIn(2000)
     
    
      
