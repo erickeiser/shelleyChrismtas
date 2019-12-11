77c1cf00d632e2793bf4ccb2dab371fa1323baf3
Eric Keiser <ekeiser@ekeiser.lan.tombras.com>
2019-12-11 12:43:12 -0500

    added jQuery

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".hero").hide()
+    $(".secondary").hide()
+    $(".button").hide()
+
+    $(".hero").delay(2000).fadeIn(1500).delay(2000).fadeOut(1500)
+    $(".secondary").delay(7500).fadeIn().delay(2000).fadeOut()
+    $(".button").delay(10000).fadeIn(1500)
+    
+   
+     
+    
+    
+})
