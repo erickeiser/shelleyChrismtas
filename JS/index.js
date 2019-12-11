073cd8e21d3641f2428a32ea4b09e8a9083be99c
Eric Keiser <ekeiser@ekeiser.lan.tombras.com>
2019-12-11 13:54:31 -0500

    added poem

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
   
 
-    $(".hero").delay(2000).fadeIn(3000).delay(2000).fadeOut(1500)
-    $(".secondary").delay(9500).fadeIn(3000).delay(3000).fadeOut(1500)
-    $(".button").delay(17500).fadeIn(2000)
+    $(".first").delay(2000).fadeIn(2000).delay(4000).fadeOut(2000)
+    $(".second").delay(10500).fadeIn(3000).delay(4000).fadeOut(1500)
+    $(".third").delay(19500).fadeIn(3000).delay(4000).fadeOut(1500)
+    $(".fourth").delay(28500).fadeIn(3000).delay(4000).fadeOut(1500)
+    $(".fifth").delay(37500).fadeIn(3000).delay(4000).fadeOut(1500)
+    $(".secondary").delay(46500).fadeIn(3000).delay(4000).fadeOut(1500)
+    $(".button").delay(55500).fadeIn(3000)
+   
     
    
      
